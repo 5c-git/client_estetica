f8b592d319b9e523f9f32ec8c541ba53b652a7d9
Alexandr Maslennikov <cray86@mail.ru>
2025-11-12 13:01:49 +0300

    Убирает билд из гитигнора.

---
+++ build/js/common.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    window.Corners5ProjectLayout.initPromoAnimation();
+    window.Corners5ProjectLayout.initPromoGalleryAnimation();
+  }, 500);
+});
