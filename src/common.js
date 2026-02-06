d86a2d32bb0b74a43f5f4aaf2d93b7b3c0eaa6a9
Alexandr Maslennikov <cray86@mail.ru>
2026-02-06 16:54:43 +0300

    Попытка пофиксить скрол в окне.

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     window.Corners5ProjectLayout.initPromoAnimation();
     window.Corners5ProjectLayout.initPromoGalleryAnimation();
